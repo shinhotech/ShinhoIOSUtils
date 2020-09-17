c82b1ca4278a131463af79dae5d9565f366a99c1
huyan_18040019 <huyan@shinhofood.com>
2020-09-17 17:14:38 +0800

    Constant

---
+++ ShinhoIOSUtils/Utils/Constant.swift
@@ -0,0 +1,65 @@
+//
+//  Constant.swift
+//  FSFA
+//
+//  Created by Lcm on 2018/2/6.
+//  Copyright © 2018年 shinho. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - layout size
+
+/// CGFloat 8.0
+let margin1: CGFloat = 8.0
+
+/// CGFloat 16.0
+let margin2: CGFloat = 16.0
+
+/// CGFloat 24.0
+let margin3: CGFloat = 24.0
+
+/// CGFloat 32.0
+let margin4: CGFloat = 32.0
+
+/// CGFloat 40.0
+let margin5: CGFloat = 40.0
+
+/// CGFloat 48.0
+let margin6: CGFloat = 48.0
+
+
+/// 页面左边距 24.0
+let leftMargin: CGFloat = 24.0
+
+/// 页面右边距 24.0
+let rightMargin: CGFloat = 24.0
+
+/** other size **/
+/// CGFloat 104.0
+let textViewHeight: CGFloat = 104.0
+
+/// photo size
+let photoSize = CGSize(width: 104, height: 104)
+
+/// bottom navgation bar height 56.0
+let navBarHeight: CGFloat = 56.0
+
+/// title bar height 67.0
+let titleBarHeight: CGFloat = 67.0
+
+/// CGFloat 104.0
+let addPhotoHeight = textViewHeight
+
+/// 最大文字数量
+let maxTextCount = 200
+/// 一页20条
+let pageSize = 20
+
+// MARK: - reuse identifier
+/// cell id
+let cellId = "cellId"
+/// header id
+let headerId = "headerId"
+/// footer id
+let footerId = "footerId"
